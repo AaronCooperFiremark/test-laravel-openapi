676adc6567ad2a82a1adffcab9d6a86fa5bad893
Aaron Cooper <73874358+AaronCooperFiremark@users.noreply.github.com>
2022-06-01 16:34:18 +0800

    commented out extra responses

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\OpenApi\RequestBodies\NoteRequestBody;
-use App\OpenApi\Responses\ClientErrorNoteResponse;
-use App\OpenApi\Responses\ServerErrorResponse;
 use App\OpenApi\Responses\SuccessNoteResponse;
 use Illuminate\Http\Request;
 use Vyuldashev\LaravelOpenApi\Attributes as OpenApi;
+
+//use App\OpenApi\Responses\ClientErrorNoteResponse;
+//use App\OpenApi\Responses\ServerErrorResponse;
 
 #[OpenApi\PathItem]
 class NotesController extends Controller
@@ -21,8 +22,8 @@ class NotesController extends Controller
     #[OpenApi\Operation]
     #[OpenApi\RequestBody(factory: NoteRequestBody::class)]
     #[OpenApi\Response(factory: SuccessNoteResponse::class, statusCode: 200)]
-    #[OpenApi\Response(factory: ClientErrorNoteResponse::class, statusCode: 400)]
-    #[OpenApi\Response(factory: ServerErrorResponse::class, statusCode: 500)]
+//    #[OpenApi\Response(factory: ClientErrorNoteResponse::class, statusCode: 400)]
+//    #[OpenApi\Response(factory: ServerErrorResponse::class, statusCode: 500)]
     public function index(Request $request): string
     {
         return response()->json([
